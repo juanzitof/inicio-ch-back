c95c6e171c4bd915bcd9ab351a2f98ec073c9ee9
juanzitof <juanzito.12@gmail.com>
2022-07-28 15:06:43 -0300

    desafio process child

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { Server as HttpServer } from "http";
 import { Server as IOServer } from "socket.io";
 import cors from "cors";
 import path from "path";
+import { yargObj } from "./utils/yargs.js";
 import { fileURLToPath } from "url";
 import MongoStore from "connect-mongo";
 import moment from "moment";
@@ -18,16 +19,18 @@ import { webRouter } from "./routes/webRouter.js";
 import { registerRouter } from "./routes/userRouter.js";
 import { loginRouter } from "./routes/userRouter.js";
 import normalizer from "./normalizr/normalizr.js";
+import { infoRouter } from "./routes/infoRouter.js";
+import { randomRouter } from "./routes/randomRouter.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+// ---- Instancia de servidor y socket -----
 const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
 
-
+// ----- Configuración Socket -----
 io.on("connection", async (socket) => {
 	console.log("Nuevo cliente conectado!");
 
@@ -50,16 +53,16 @@ io.on("connection", async (socket) => {
 	});
 });
 
-
+// ----- Configuración Server -----
 app.use(json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(cors(`${config.cors}`));
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // registra el motor de plantillas
 
-
+// ----- Rutas -----
 app.use(
 	session({
 		store: MongoStore.create({
@@ -82,9 +85,11 @@ app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 app.use("/api/productos", productsRouter);
 app.use("/api/carrito", cartRouter);
+app.use("/info", infoRouter);
+app.use("/api/randoms", randomRouter);
 app.use("/", webRouter);
 
-const PORT = config.PORT;
+const PORT = yargObj.PORT;
 const server = httpServer.listen(PORT, () => {
 	console.log(`Express is listening in port http://localhost:${PORT}`);
 });
